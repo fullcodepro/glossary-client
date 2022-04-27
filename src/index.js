475caed0871740215e4b2363fc25d9e0ca3017c7
aalesann <alejandrosanabria74@gmail.com>
2022-04-27 01:44:09 -0300

    Some fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { DragAndDrop } from './components/extras/DragAndDrop';
 
 ReactDOM.render(
-    <DragAndDrop />,
+    <App />,
   document.getElementById('root')
 );
 
